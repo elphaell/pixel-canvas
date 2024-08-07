2fa102758c57ffc91932d0f65879f1278fc11df9
elphaell <70542480+elphaell@users.noreply.github.com>
2024-08-07 21:59:50 +0100

    feat: add darkenColour and pSBC fucntions

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 const pixelCanvas = document.querySelector("#canvas");
 const pixelCanvasSize = 400;
 
+// pSBC - Shade Blend Convert - Version 4.1 - 01/7/2021
+// https://github.com/PimpTrizkit/PJs/blob/master/pSBC.js
+
+const pSBC=(p,c0,c1,l)=>{
+	let r,g,b,P,f,t,h,m=Math.round,a=typeof(c1)=="string";
+	if(typeof(p)!="number"||p<-1||p>1||typeof(c0)!="string"||(c0[0]!='r'&&c0[0]!='#')||(c1&&!a))return null;
+	h=c0.length>9,h=a?c1.length>9?true:c1=="c"?!h:false:h,f=pSBC.pSBCr(c0),P=p<0,t=c1&&c1!="c"?pSBC.pSBCr(c1):P?{r:0,g:0,b:0,a:-1}:{r:255,g:255,b:255,a:-1},p=P?p*-1:p,P=1-p;
+	if(!f||!t)return null;
+	if(l)r=m(P*f.r+p*t.r),g=m(P*f.g+p*t.g),b=m(P*f.b+p*t.b);
+	else r=m((P*f.r**2+p*t.r**2)**0.5),g=m((P*f.g**2+p*t.g**2)**0.5),b=m((P*f.b**2+p*t.b**2)**0.5);
+	a=f.a,t=t.a,f=a>=0||t>=0,a=f?a<0?t:t<0?a:a*P+t*p:0;
+	if(h)return"rgb"+(f?"a(":"(")+r+","+g+","+b+(f?","+m(a*1000)/1000:"")+")";
+	else return"#"+(4294967296+r*16777216+g*65536+b*256+(f?m(a*255):0)).toString(16).slice(1,f?undefined:-2)
+}
+
+pSBC.pSBCr=(d)=>{
+	const i=parseInt;
+	let n=d.length,x={};
+	if(n>9){
+		const [r, g, b, a] = (d = d.split(','));
+	        n = d.length;
+		if(n<3||n>4)return null;
+		x.r=i(r[3]=="a"?r.slice(5):r.slice(4)),x.g=i(g),x.b=i(b),x.a=a?parseFloat(a):-1
+	}else{
+		if(n==8||n==6||n<4)return null;
+		if(n<6)d="#"+d[1]+d[1]+d[2]+d[2]+d[3]+d[3]+(n>4?d[4]+d[4]:"");
+		d=i(d.slice(1),16);
+		if(n==9||n==5)x.r=d>>24&255,x.g=d>>16&255,x.b=d>>8&255,x.a=Math.round((d&255)/0.255)/1000;
+		else x.r=d>>16,x.g=d>>8&255,x.b=d&255,x.a=-1
+	}return x
+};
+
 function generateGrid() {
 
     // clear the grid first!!
@@ -19,6 +51,7 @@ function generateGrid() {
             let canvasItem = document.createElement("div");
             canvasItem.style.flex = "auto";
             canvasItem.style.border = "1px solid black";
+            canvasItem.style.backgroundColor = "rgb(255, 255, 255)";
             canvasItem.classList.add("pixel-cell");
             canvasRow.append(canvasItem);
         }
@@ -27,6 +60,8 @@ function generateGrid() {
     }
 }
 
+// TODO Refactor into mode selector
+// TODO Remove hard coded colours
 function solidColour() {
     let cellList = document.querySelectorAll(".pixel-cell");
 
@@ -40,11 +75,28 @@ function solidColour() {
 }
 
 
+function darkenColour() {
+    let cellList = document.querySelectorAll(".pixel-cell");
+
+    cellList.forEach((cell) => {
+
+        let visitedTimes = 0;
+
+        cell.addEventListener("mouseenter", (e) => {
+
+            currentColour = cell.style.backgroundColor;
+            if (visitedTimes < 10) { visitedTimes++ };
+
+            cell.style.backgroundColor = pSBC(-0.1 * visitedTimes, "rgb(255, 255, 255)", "rgb(255, 0, 0)");
+        })
+    })
+}
+
 // tester button
 // TODO refactor to update the input dynamically
 // consider using a slider
 const btn = document.querySelector("#btn");
 btn.onclick = () => {
     generateGrid();
-    solidColour();
+    darkenColour();
 };
